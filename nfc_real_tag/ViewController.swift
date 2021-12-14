2362e925741682940b79efe0094e266a1bcf00ff
KOSSHOH <shahboz.turonov98@gmail.com>
2021-12-14 14:30:43 +0500

    Initial Commit

---
+++ nfc_real_tag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nfc_real_tag
+//
+//  Created by Shahboz Turonov on 14/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nfc_real_tagUITests/nfc_real_tagUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nfc_real_tagUITestsLaunchTests.swift
+//  nfc_real_tagUITests
+//
+//  Created by Shahboz Turonov on 14/12/21.
+//
+
+import XCTest
+
+class nfc_real_tagUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
